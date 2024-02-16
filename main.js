df4329e7c67f28a64760957b571835bbfc10184c
venugopalmavilla <venugopalmavilla9@gmail.com>
2024-02-16 16:49:02 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+        <p class="fw-medium"><a class="#" href=".show" style="text-decoration: none; color: #222222;"> » Data and information handling</a></p>
+        <p class="fw-medium"><a class="#" href="#" style="text-decoration: none; color: #222222;"> » Operating systems and computer architecture</a</p>
+        <p class="fw-medium"><a class="#" href="#" style="text-decoration: none; color: #222222;"> » Accelerated graphics port</a></p>
+        <p class="fw-medium"><a class="#" href="#" style="text-decoration: none; color: #222222;"> » Microprocessor and microcontrollers</a></p>
+
+        <h3 class="featurette-heading fw-400 lh-2" style="color: #ff9800"><b>SPOT EVENTS</b></h3>
+        <p class="fw-medium"><a class="#" href="#" style="text-decoration: none; color: #222222;"> » Code buzz</a></p>
+        <p class="fw-medium"><a class="#" href="#" style="text-decoration: none; color: #222222;"> » smart quizz</a</p>
+        <p class="fw-medium"><a class="#" href="#" style="text-decoration: none; color: #222222;"> » Poster presentaion</a></p>
